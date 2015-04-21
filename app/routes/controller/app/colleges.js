ae646ff364c8c442c516a62cc1749eadb4d2698d
al285 <al285@njit.edu>
2015-04-21 19:45:29 -0400

    Create colleges.js

---
+++ app/routes/controller/app/colleges.js
@@ -0,0 +1,19 @@
+var config = require('../../../../config'),
+    MongoClient = require('mongodb').MongoClient,
+    College = require('../../../models/College');
+
+module.exports = function(req, res) {
+    College.find({}, function(err, data) {
+        if (err) throw err;
+        if (data.length > 0) {
+            res.render('listColleges', {
+                title: 'List of Colleges',
+                colleges: data[0].college
+            });
+        } else {
+            res.render('uploadFile', {
+                title: 'File Upload Page',
+            });
+        }
+    });
+};
